f07731f8be258ccd77512eb6a2b2e717581d58f6
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-03-09 15:17:25 +0200

    march09032023

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryWrapEl = document.querySelector('.gallery');
+const cardsImg = creatImgCards(galleryItems);
+galleryWrapEl.insertAdjacentHTML('beforeend', cardsImg);
+
+galleryWrapEl.addEventListener('click', onImgContainerClick);
+function creatImgCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+const galleryIMG = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(galleryIMG);
